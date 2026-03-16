a3508b44bd416f30726aba10d8cae9988de3d618
najma manaf <najmamanaf18@gmail.com>
2026-03-16 12:33:37 +0530

    backend url added

--- utils/api.js
+++ utils/api.js
@@ -3,9 +3,6 @@ import axios from "axios"
 const API = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json"
-  }
 })
 
 export default API
