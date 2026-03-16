6c031728276ac9fbde7998dbe6021c0519e0da34
najma manaf <najmamanaf18@gmail.com>
2026-03-16 12:48:02 +0530

    chaNGE VISIBLE

--- utils/api.js
+++ utils/api.js
@@ -2,6 +2,7 @@ import axios from "axios"
 
 const API = axios.create({
   baseURL: "https://taskmanager-backend-1rgj.onrender.com"|| process.env.NEXT_PUBLIC_API_URL,
+  withCredentials:true,
   headers: {
     "Content-Type": "application/json"
   }
