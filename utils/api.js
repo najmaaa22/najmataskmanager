6aad9a0e2bd632bc21ad873f1b340991fb8ddd95
najma manaf <najmamanaf18@gmail.com>
2026-03-16 10:38:33 +0530

    backend url added

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const API = axios.create({
- baseURL:"http://localhost:5000/api"
+ baseURL:"https://taskmanager-backend-1rgj.onrender.com/api",
+ 
 })
 
 export default API
